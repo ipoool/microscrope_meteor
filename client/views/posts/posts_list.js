f78a386809024a27109bb74828832c948d967959
Contrivent <maverick@Macintosh-3.local>
2014-12-28 05:44:43 +0700

    bisa add dengan mongo

--- client/views/posts/posts_list.js
+++ client/views/posts/posts_list.js
@@ -1,21 +1,19 @@
-var postsData = [
-  {
-    title: 'Introducing Telescope',
-    author: 'Sacha Greif',
-    url: 'http://sachagreif.com/introducing-telescope/'
-  }, 
-  {
-    title: 'Meteor',
-    author: 'Tom Coleman',
-    url: 'http://meteor.com'
-  }, 
-  {
-    title: 'The Meteor Book',
-    author: 'Tom Coleman',
-    url: 'http://themeteorbook.com'
-  }
-];
 
 Template.postsList.helpers({
-  posts: postsData
+  posts: function(){
+     return Posts.find();
+  }
+});
+
+Template.body.events({
+  "submit .new-post":function(){
+     var title = event.target.title.value;
+     Posts.insert({
+       title: title,
+       url:"http://code4war.com"
+     });
+     event.target.title.value = "";
+
+     return false;
+  }
 });
